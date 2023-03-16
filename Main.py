43bbdf3fff13b831d1883196602e3f04284daf4f
oxyfoxys <oxyfoxys@yandex.ru>
2023-03-16 15:50:04 +0300

    exercise(15.3/5/6)

--- Main.py
+++ Main.py
@@ -619,7 +619,62 @@ turtle.goto(BASE_RIGHT_X, BASE_RIGHT_Y)
 turtle.end_fill()
 '''
 
+'''
 #3
+import turtle
+
+TOP_SQUARE_TOP_LEFT_X = -100
+TOP_SQUARE_TOP_LEFT_Y = 100
+TOP_SQUARE_TOP_RIGHT_X = 0
+TOP_SQUARE_TOP_RIGHT_Y = 100
+TOP_SQUARE_BOTTOM_LEFT_X = -100
+TOP_SQUARE_BOTTOM_LEFT_Y = 0
+TOP_SQUARE_BOTTOM_RIGHT_X = 0
+TOP_SQUARE_BOTTOM_RIGHT_Y = 0
+
+BOTTOM_SQUARE_TOP_LEFT_X = 0
+BOTTOM_SQUARE_TOP_LEFT_Y = 0
+BOTTOM_SQUARE_TOP_RIGHT_X = 100
+BOTTOM_SQUARE_TOP_RIGHT_Y = 0
+BOTTOM_SQUARE_BOTTOM_RIGHT_X = 100
+BOTTOM_SQUARE_BOTTOM_RIGHT_Y = -100
+BOTTOM_SQUARE_BOTTOM_LEFT_X = 0
+BOTTOM_SQUARE_BOTTOM_LEFT_Y = -100
+
+turtle.hideturtle()
+turtle.penup()
+
+# Начертить самый верхний квадрат
+turtle.goto(TOP_SQUARE_BOTTOM_RIGHT_X, TOP_SQUARE_BOTTOM_RIGHT_Y)
+turtle.pendown()
+turtle.goto(TOP_SQUARE_TOP_RIGHT_X, TOP_SQUARE_TOP_RIGHT_Y)
+turtle.goto(TOP_SQUARE_TOP_LEFT_X, TOP_SQUARE_TOP_LEFT_Y)
+turtle.goto(TOP_SQUARE_BOTTOM_LEFT_X, TOP_SQUARE_BOTTOM_LEFT_Y)
+turtle.goto(TOP_SQUARE_BOTTOM_RIGHT_X, TOP_SQUARE_BOTTOM_RIGHT_Y)
+turtle.penup()
+
+# Начертить нижний квадрат
+turtle.goto(BOTTOM_SQUARE_BOTTOM_RIGHT_X, BOTTOM_SQUARE_BOTTOM_RIGHT_Y)
+turtle.pendown()
+turtle.goto(BOTTOM_SQUARE_TOP_RIGHT_X, BOTTOM_SQUARE_TOP_RIGHT_Y)
+turtle.goto(BOTTOM_SQUARE_TOP_LEFT_X, BOTTOM_SQUARE_TOP_LEFT_Y)
+turtle.goto(BOTTOM_SQUARE_BOTTOM_LEFT_X, BOTTOM_SQUARE_BOTTOM_LEFT_Y)
+turtle.goto(BOTTOM_SQUARE_BOTTOM_RIGHT_X, BOTTOM_SQUARE_BOTTOM_RIGHT_Y)
+turtle.penup()
+
+# Соединить углы
+turtle.goto(BOTTOM_SQUARE_BOTTOM_RIGHT_X, BOTTOM_SQUARE_BOTTOM_RIGHT_Y)
+turtle.pendown()
+turtle.goto(TOP_SQUARE_TOP_LEFT_X, TOP_SQUARE_TOP_LEFT_Y)
+turtle.penup()
+turtle.goto(BOTTOM_SQUARE_TOP_RIGHT_X, BOTTOM_SQUARE_TOP_RIGHT_Y)
+turtle.pendown()
+turtle.goto(TOP_SQUARE_TOP_RIGHT_X, TOP_SQUARE_TOP_RIGHT_Y)
+turtle.penup()
+turtle.goto(BOTTOM_SQUARE_BOTTOM_LEFT_X, BOTTOM_SQUARE_BOTTOM_LEFT_Y)
+turtle.pendown()
+turtle.goto(TOP_SQUARE_BOTTOM_LEFT_X, TOP_SQUARE_BOTTOM_LEFT_Y)
+'''
 
 #4
 '''
@@ -670,4 +725,179 @@ turtle.penup()
 turtle.goto(x, y)
 turtle.pendown()
 turtle.circle(RADIUS)
+'''
+
+#5
+'''
+import turtle
+
+CENTER_X = 0
+CENTER_Y = 0
+X_AXIS_LENGTH = 200
+Y_AXIS_LENGTH = 200
+RADIUS = 25
+SOUTH = 270
+EAST = 0
+
+# Спрятать черепаху и задать скорость анимации.
+turtle.hideturtle()
+turtle.speed(0)
+
+# Начертить ось X
+x = CENTER_X - (X_AXIS_LENGTH / 2)
+y = CENTER_Y
+turtle.penup()
+turtle.goto(x, y)
+turtle.pendown()
+turtle.forward(X_AXIS_LENGTH)
+
+# Начертить ось Y
+x = CENTER_X
+y = CENTER_Y + (Y_AXIS_LENGTH / 2)
+turtle.penup()
+turtle.goto(x, y)
+turtle.pendown()
+turtle.setheading(SOUTH)
+turtle.forward(X_AXIS_LENGTH)
+
+# Начертить центральный круг
+x = CENTER_X
+y = CENTER_Y - RADIUS
+turtle.penup()
+turtle.setheading(EAST)
+turtle.goto(x, y)
+turtle.pendown()
+turtle.circle(RADIUS)
+
+# Написать "Север"
+x = CENTER_X - 10
+y = CENTER_Y + (Y_AXIS_LENGTH / 2)
+turtle.penup()
+turtle.goto(x, y)
+turtle.pendown()
+turtle.write("Север")
+
+# Написать "Юг"
+x = CENTER_X - 10
+y = CENTER_Y - (Y_AXIS_LENGTH / 2) - 10
+turtle.penup()
+turtle.goto(x, y)
+turtle.pendown()
+turtle.write("Юг")
+
+# Написать "Запад"
+x = CENTER_X - (X_AXIS_LENGTH / 2) - 25
+y = CENTER_Y - 7
+turtle.penup()
+turtle.goto(x, y)
+turtle.pendown()
+turtle.write("Запад")
+
+# Написать "Восток"
+x = CENTER_X + (X_AXIS_LENGTH / 2) + 2
+y = CENTER_Y - 7
+turtle.penup()
+turtle.goto(x, y)
+turtle.pendown()
+turtle.write("Восток")
+'''
+
+#6
+'''
+import turtle
+
+UPPER_LEFT_X = -100
+UPPER_LEFT_Y = 100
+UPPER_RIGHT_X = 100
+UPPER_RIGHT_Y = 100
+LOWER_LEFT_X = -100
+LOWER_LEFT_Y = -100
+LOWER_RIGHT_X = 100
+LOWER_RIGHT_Y = -100
+CENTER_X = 0
+CENTER_Y = 0
+GAP = 20
+
+# Спрятать черепаху и задать скорость анимации.
+turtle.hideturtle()
+turtle.speed(0)
+
+# Начертить сплошные линии.
+turtle.penup()
+turtle.goto(UPPER_LEFT_X, UPPER_LEFT_Y)
+turtle.pendown()
+turtle.goto(LOWER_RIGHT_X, LOWER_RIGHT_Y)
+turtle.penup()
+turtle.goto(UPPER_RIGHT_X, UPPER_RIGHT_Y)
+turtle.pendown()
+turtle.goto(LOWER_LEFT_X, LOWER_LEFT_Y)
+turtle.penup()
+turtle.goto(UPPER_LEFT_X, UPPER_LEFT_Y)
+turtle.pendown()
+turtle.goto(LOWER_LEFT_X, LOWER_LEFT_Y)
+turtle.penup()
+turtle.goto(UPPER_RIGHT_X, UPPER_RIGHT_Y)
+turtle.pendown()
+turtle.goto(LOWER_RIGHT_X, LOWER_RIGHT_Y)
+
+# Начертить вершнюю пунктирную линию.
+turtle.penup()
+turtle.goto(UPPER_LEFT_X, UPPER_LEFT_Y)
+turtle.pendown()
+turtle.forward(GAP)
+turtle.penup()
+turtle.forward(GAP)
+turtle.pendown()
+turtle.forward(GAP)
+turtle.penup()
+turtle.forward(GAP)
+turtle.pendown()
+turtle.forward(GAP)
+turtle.penup()
+turtle.forward(GAP)
+turtle.pendown()
+turtle.forward(GAP)
+turtle.penup()
+turtle.forward(GAP)
+turtle.pendown()
+turtle.forward(GAP)
+turtle.penup()
+turtle.forward(GAP)
+
+# Начертить нижнюю пунктирную линию.
+turtle.penup()
+turtle.goto(LOWER_LEFT_X, LOWER_LEFT_Y)
+turtle.pendown()
+turtle.forward(GAP)
+turtle.penup()
+turtle.forward(GAP)
+turtle.pendown()
+turtle.forward(GAP)
+turtle.penup()
+turtle.forward(GAP)
+turtle.pendown()
+turtle.forward(GAP)
+turtle.penup()
+turtle.forward(GAP)
+turtle.pendown()
+turtle.forward(GAP)
+turtle.penup()
+turtle.forward(GAP)
+turtle.pendown()
+turtle.forward(GAP)
+turtle.penup()
+turtle.forward(GAP)
+
+# Начертить точки.
+turtle.penup()
+turtle.goto(UPPER_LEFT_X, UPPER_LEFT_Y)
+turtle.dot()
+turtle.goto(UPPER_RIGHT_X, UPPER_RIGHT_Y)
+turtle.dot()
+turtle.goto(LOWER_LEFT_X, LOWER_LEFT_Y)
+turtle.dot()
+turtle.goto(LOWER_RIGHT_X, LOWER_RIGHT_Y)
+turtle.dot()
+turtle.goto(CENTER_X, CENTER_Y)
+turtle.dot()
 '''
